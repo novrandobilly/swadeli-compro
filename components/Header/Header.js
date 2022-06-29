115887dc86cb27753ca4825f401dc962a6af6198
Renes Dwi Riwanto <renesriwanto@gmail.com>
2022-06-29 21:53:12 +0700

    header comp

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
+import Image from "next/image";
+import Logo from "../../assets/Logo.png";
+import styless from "./Header.module.scss";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={styless["header"]}>
+      <div>
+        <Image src={Logo} className={styless["logo"]} width={264} height={55} />
+      </div>
+      <nav className={styless["navigation"]}>
+        <a>Join us</a>
+        <a>FAQ</a>
+        <a>About us</a>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
-import Head from 'next/head';
-import styles from './Home.module.scss';
+import Head from "next/head";
+import Header from "../components/Header/Header";
+import styles from "./Home.module.scss";
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Swadeli</title>
-        <meta name="description" content="Swadeli Quick Commerce App. Groceries and time, at your hands. " />
+        <meta
+          name="description"
+          content="Swadeli Quick Commerce App. Groceries and time, at your hands. "
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className={styles['title']}>Swadeli</h1>
+        <Header />
       </main>
     </div>
   );
