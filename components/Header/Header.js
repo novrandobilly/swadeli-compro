3019aa6c40e14ec77a2e04b8d3ac8cb49c6ccff9
Renes Dwi Riwanto <renesriwanto@gmail.com>
2022-07-01 23:55:24 +0700

    header-done

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,19 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Logo from "../../assets/Logo.png";
-import styless from "./Header.module.scss";
+import styles from "./Header.module.scss";
+import Link from "next/link";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { CgClose } from "react-icons/cg";
 
+const hamburger = () => {
+  active ? (
+    <CgClose className={styles["menu"]} onClick={() => setActive(!active)} />
+  ) : (
+    <GiHamburgerMenu
+      className={styles["menu"]}
+      onClick={() => setActive(!active)}
+    />
+  );
+};
 const Header = () => {
+  const [active, setActive] = useState(false);
   return (
-    <div className={styless["header"]}>
-      <div>
-        <Image src={Logo} className={styless["logo"]} width={264} height={55} />
-      </div>
-      <nav className={styless["navigation"]}>
-        <a>Join us</a>
-        <a>FAQ</a>
-        <a>About us</a>
+    <div className={`${styles.header}`}>
+      <Image src={Logo} width={140} height={33} className={styles["logo"]} />
+      <nav className={`${styles["navigation"]} ${active ? styles.temp : ""}`}>
+        <Link href="/joinus">
+          <a>Join us</a>
+        </Link>
+        <Link href="/faq">
+          <a>FAQ</a>
+        </Link>
+        <Link href="/about">
+          <a>About us</a>
+        </Link>
       </nav>
+      {active ? (
+        <CgClose
+          className={styles["menu"]}
+          onClick={() => setActive(!active)}
+        />
+      ) : (
+        <GiHamburgerMenu
+          className={styles["menu"]}
+          onClick={() => setActive(!active)}
+        />
+      )}
     </div>
   );
 };
